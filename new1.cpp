de54a2a0bed3a2716c42b664b8642700d17a4ec5
Manjunathshetty <64469369+Manjunathshetty@users.noreply.github.com>
2020-05-15 23:40:11 +0530

    Update new1.cpp

--- new1.cpp
+++ new1.cpp
@@ -1,15 +1,12 @@
     	#include<iostream>
     	using namespace std;
+        int main(){
     	int n,i;
     	cout<<enter value of n:<<n;
-    	for (i=0;i<=n,i++)
-    	{
-    	if(i==1)
-    	{
-    	cout<<i;
-    	}
-    	else{
-    	i=((n*n)+1)
+    	for (i=1;i<=n,i++)
+        {
+    	i=((2*i)-1)
     	cout<<i<<",";
     	}
+            return 0;
     	}
